f7371d335e76f7e0f916113a0dbfa97f407744e5
tema2024 <163888751+tema2024@users.noreply.github.com>
2024-05-18 12:02:39 +0300

    end

---
+++ HH.py
@@ -0,0 +1,39 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+url = "https://tomsk.hh.ru/vacancies/programmist"
+driver.get(url)
+
+time.sleep(3)
+
+vacancies = driver.find_elements(By.CLASS_NAME, 'vacancy-card--H8LvOiOGPll0jZvYpxIF')
+
+print(vacancies)
+parsed_data = []
+
+for vacancy in vacancies:
+    try:
+        title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--SYbxrgpHgHedVTkgI_cA').text
+        company = vacancy.find_element(By.CSS_SELECTOR, 'span.company-info-text--O32pGCRW0YDmp3BHuNOP').text
+        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--cCPBXayRjn5GuLFWhGTJ').text
+        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
+        parsed_data.append([title, company, salary, link])
+    except:
+        print(f'произошла ошибка при парсинге:')
+        continue
+
+driver.quit()
+
+# Write to CSV file
+with open("hh.csv", mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Название вакансии', 'Название компании', 'Зарплата', 'Ссылка на вакансию'])
+    writer.writerows(parsed_data)
+
+
+
+
